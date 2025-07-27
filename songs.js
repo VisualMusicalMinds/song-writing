1b1c6953ea4e1f49ea3f3c0dabac0849392065a4
Nathan <visualmusicalminds@gmail.com>
2025-07-27 19:44:14 -0400

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,14 @@
+const PRELOADED_SONGS = {
+    'twinkle': `[Key of C]
+[A]
+Twin[D1] kle,[D1] twin[S1] kle,[S1] lit[L1] tle[L1] star[S1] How[F1] I[F1] won[M1] der[M1] what[R1] you[R1] are[D1]
+[B]
+Up[S1] a-[S1] bove[F1] the[F1] world[M1] so[M1] high,[R1] Like[S1] a[S1] dia-[F1] mond[F1] in[M1] the[M1] sky.[R1]
+[A]
+Twin[D1] kle,[D1] twin[S1] kle,[S1] lit[L1] tle[L1] star,[S1] How[F1] I[F1] won[M1] der[M1] what[R1] you[R1] are.[D1]`,
+    'mary': `[Key of C]
+[A]
+Ma[M1] ry[R1] had[D1] a[R1] lit[M1] tle[M1] lamb,[M1] lit[R1] tle[R1] lamb,[R1] lit[M1] tle[S1] lamb.[S1]
+[B]
+Ma[M1] ry[R1] had[D1] a[R1] lit[M1] tle[M1] lamb,[M1] its[M1] fleece[R1] was[R1] white[M1] as[R1] snow.[D1]`
+};
