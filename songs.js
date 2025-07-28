3ce0e3f282e992bd09f4999af16eeb8d9bc68ae5
Nathan <visualmusicalminds@gmail.com>
2025-07-28 19:35:05 -0400

    Update songs.js

--- songs.js
+++ songs.js
@@ -11,23 +11,21 @@ Twin[D1] kle,[D1] twin[S1] kle,[S1] lit[L1] tle[L1] star,[S1] How[F1] I[F1] won[
 Ma[M1] ry[R1] had[D1] a[R1] lit[M1] tle[M1] lamb,[M1] lit[R1] tle[R1] lamb,[R1] lit[M1] tle[S1] lamb.[S1]
 [B]
 Ma[M1] ry[R1] had[D1] a[R1] lit[M1] tle[M1] lamb,[M1] its[M1] fleece[R1] was[R1] white[M1] as[R1] snow.[D1]`,
-
-   'starspangledbanner': `[Key of Ab]
+    'starspangledbanner': `[Key of Ab]
 [A1]
-Oh[S1] -[M1] say[D1] can[M1] you[S1] see[D2] By[M2] the[R2] dawn's[D2] ear-[M1] -ly[F#1] light?[S1]
+Oh[S-1] say[M-1] can[D1] you[M1] see,[S1] by[D2] the[T1] dawn's[D2] ear[L1]ly[F#1] light,[S1]
 [A2]
-What[S1] so[S1] proud-[M2] -ly[R2] we[D2] hailed[T1] At[L1] the[T1] twi-[D2] lights[D2] last[S1] gleam-[M1] ing[D1]
+What[S1] so[S1] proud[M2]ly[R2] we[D2] hailed[T1] at[L1] the[T1] twi[D2]light's[D2] last[S1] gleam[M1]ing?[D1]
 [A1]
-Whose[S1] broad[M1] stripes[D1] and[M1] bright[S1] stars[D2] Through[M2] the[R2] pe-[D2] -ril-[M1] -ous[F#1] fight[S1]
+Whose[S1] broad[M1] stripes[D1] and[M1] bright[S1] stars,[D2] through[M2] the[R2] per[D2]il[L1]ous[F#1] fight,[S1]
 [A2]
-O'er[S1] the[S1] ram-[M2] -parts[R2] we[D2] watched,[T1] Were[L1] so[T1] gal-[D2] -lant-[D2] -ly[S1] stream-[M1] -ing[D1]
+O'er[S1] the[S1] ram[M2]parts[R2] we[D2] watched[T1] were[L1] so[T1] gal[D2]lant[D2]ly[S1] stream[M1]ing?[D1]
 [B]
-And[M2] the[M2] roc-[M2] -kets[F2] red[S2] glare[S2] The[F2] bombs[M2] burst-[R2] -ing[M2] in[F2] air[F2]
+And[M2] the[M2] rock[M2]et's[F2] red[S2] glare,[S2] the[F2] bombs[M2] burst[R2]ing[M2] in[F2] air,[F2]
 [New Line]
-Gave[F2] proof[M2] through[R2] the[D2] night[T1] That[L1] our[T1] flag[D2] was[M1] still[F#1] there[S1]
+Gave[F2] proof[M2] through[R2] the[D2] night[T1] that[L1] our[T1] flag[D2] was[M1] still[F#1] there.[S1]
 [C]
-Oh,[S1] say,[D2] does[D2] that[D2] -[T1] Star[L1] Span-[L1] -gled[L1] Ban-[R2] -ner__[F2] ___[M2] yet[R2] -[D2] wave?[D2] ___[T1]
+O[S1] say[D2] does[D2] that[T1] star[L1]span[L1]gled[L1] ban[R2]ner[F2] yet[M2] wave[D2]
 [New Line]
-O'er[S1] the[S1] land[D2] -[R2] of[M2] the[F2] free[S2] And[D2] the[R2] home[M2] of[F2] the[R2] brave?[D2]
-
+O'er[S1] the[S1] land[D2] of[R2] the[M2] free,[F2] and[S2] the[D2] home[R2] of[M2] the[F2] brave?[D2]`
 };
